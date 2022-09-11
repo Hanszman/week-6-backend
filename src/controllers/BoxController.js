4d84662e458ce12fd41741dbfd421dc682fc4514
hanszman <victor.hanszman@hotmail.com>
2022-09-11 00:03:27 -0300

    ordenação

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -11,7 +11,10 @@ class BoxController {
 
     // Visualização de uma Box e seus Files
     async show (req, res) {
-        const box = await Box.findById(req.params.id).populate('files'); // O populate serve para não retornar apenas o id dos files, como também os outros campos deles
+        const box = await Box.findById(req.params.id).populate({ // O populate serve para não retornar apenas o id dos files, como também os outros campos deles
+            path: 'files',
+            options: { sort: { createdAt : -1 } } // Ordenação pela data de criação, -1 é decrescente, do mais recente para o mais antigo
+        });
         return res.json(box);
     }
 }
